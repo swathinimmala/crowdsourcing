afe7cc102039460768e71839d682bbdb6e680aa9
Aadil <aadil@aadil-desktop.(none)>
2010-12-28 12:16:15 +0530

    scaffolded jobs model sini helped in the prosess

--- jobposter/db/schema.rb
+++ jobposter/db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101223113518) do
+ActiveRecord::Schema.define(:version => 20101228064030) do
+
+  create_table "jobs", :force => true do |t|
+    t.string   "description"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
